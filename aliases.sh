6e7b7d60b8242c8342fea92e56e80884746ed192
DavidLiu <dvdgege5@gmail.com>
2018-03-03 15:48:01 -0800

    fix alias appending

--- aliases.sh
+++ aliases.sh
@@ -1,12 +1,19 @@
 echo "Setup bash aliases by writing them to .bashrc"
 echo "restart terminals after this is finished, or run source ~/.bashrc"
 
-cat >> ~/.bashrc
+echo "\n\n\n"  >> ~/.bashrc
 
 # my aliases:
 
 # less, starting reading from the end. Useful for reading most recent history
-alias lessg="less +G"
+echo 'alias lessg="less +G" ' >> ~/.bashrc
 
 # quick search for file recursively in current and sub dirs
-alias ff="find . -iname "
+echo 'alias ff="find . -iname "' >> ~/.bashrc
+
+# disable trackpad acceleration
+xset q | grep -A 1 Pointer
+xset m 0/1 4
+
+source ~/.bashrc
+
