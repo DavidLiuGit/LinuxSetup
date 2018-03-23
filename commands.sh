f1381a861a983c45d014491efab8007af6fe4cf8
DL <dvdgege5@gmail.com>
2018-03-23 01:04:34 -0700

    add port redirect command

--- commands.sh
+++ commands.sh
@@ -1,3 +1,6 @@
 # firewall
 sudo ufw status verbose			# view firewall status
 sudo ufw allow 53			# allow connection on port (both TCP & UDP)
+
+# redirect port with iptables
+sudo iptables -t nat -A OUTPUT -o lo -p tcp --dport 6970 -j REDIRECT --to-port 3000
